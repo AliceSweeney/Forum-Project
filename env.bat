c4057e311fa89bf4743e6e4480d201f38be1b942
AliceSweeney <alice@sweeneyclan.org>
2025-12-05 13:24:18 -0800

    Delete env.bat

--- env.bat
+++
@@ -1,5 +0,0 @@
-set MONGO_CONNECTION_STRING=mongodb+srv://User0:a43yVobGtO1MRCAb@cluster0.igoamwn.mongodb.net/?appName=Cluster0
-set MONGO_DBNAME=ForumDB
-set GITHUB_CLIENT_ID=Ov23libefqxrkwwqYWTb
-set GITHUB_CLIENT_SECRET=e14da2d10cd7ed79f23e674bcef48376581fad06
-set SECRET_KEY=asdf
